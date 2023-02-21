9ecd5735d247935af6c83fa778673789f7ed642e
rog <rj.wkht123@gmail.com>
2023-02-21 13:30:57 +0900

    new change

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class ModelName(str , Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+
 
 
 @app.get("/")
@@ -30,4 +39,20 @@ async def read_item(skip: int =0 , limit:int =7):
     return fake_items_db[skip:skip+limit]
 
 
-   
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
+
+
+
+
+
